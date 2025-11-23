522cb4f514242ef5ede9bd7792a0b27e4578d66a
ahmed tarek <amedtarekamedtarek@gmail.com>
2025-11-23 14:03:19 +0200

    day 7

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -10,46 +10,11 @@ Route::get('/contact', function () {
     return view('contact');
 });
 Route::get('/jobs', function () {
-    return view('jobs',[
-        'jobs' =>[
-            [
-                'id' => '1',
-                'title'=> 'Director',
-                'salary'=> '50,000',
-            ],
-            [
-                'id' => '2',
-                'title' => 'Programmer',
-                'salary' => '90,000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'Teacher',
-                'salary' => '60,000'
-            ]
-        ]
-    ]
+    return view('jobs',['jobs'=>Job::all()]
     );
 });
 
 Route::get('/job/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => '1',
-            'title' => 'Director',
-            'salary' => '50,000'
-        ],
-        [
-            'id' => '2',
-            'title' => 'Programmer',
-            'salary' => '90,000'
-        ],
-        [
-            'id' => '3',
-            'title' => 'Teacher',
-            'salary' => '60,000'
-        ]
-    ];
-    $job = Arr::first($jobs,fn($job) => $job['id'] == $id,'undfind id');
+    $job = Job::find($id);
     return view('job', ['job'=> $job]);
 });

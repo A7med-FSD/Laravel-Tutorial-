1f3cf2d6a94d67235a3fc12d1ef2b89f6f9d0ec0
ahmed tarek <amedtarekamedtarek@gmail.com>
2025-11-25 11:43:18 +0200

    day 11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends model {
     use HasFactory;
     protected $table = 'job_listings';
+
+    protected $fillable = ['title','salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
 }
